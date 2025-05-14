eebb5fe6f857b71068a840d0239157e54754f08f
VitRodriguesB <148454457+VitRodriguesB@users.noreply.github.com>
2025-05-14 20:23:17 -0300

    up

--- Lista_Exercicio/lista2.exer9.sql
+++ Lista_Exercicio/lista2.exer9.sql
@@ -1,7 +1,9 @@
+-- sub query para select e retornar listas de dados crusados
 select h.*,
 (select p.nome from proprietarios p
 where p.proprietario_id = h.proprietario_id) as nome 
 from hospedagens h;
+
 
 select * from clientes c where c.cliente_id not in (select a.cliente_id from alugueis a);
 
